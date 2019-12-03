dadd5d805bc64aaea84ac30817fda458a11e431c
roberto.elero <roberto.elero@ifood.com.br>
2019-12-03 20:26:29 -0300

    Improving structure.

--- node-server/index.js
+++ node-server/index.js
@@ -1,8 +1,9 @@
 var http = require('http')
 
-const PORT = 8080
+const PORT = process.env.APP_PORT || 8080
 
 http.createServer((req, res) => {
+  res.writeHead(500, {'Content-Type': 'application/text'})
   let body = `Success from server ${PORT}`
   res.write(body)
   res.end()
